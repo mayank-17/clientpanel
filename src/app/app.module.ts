be9c21a6679845512bfa9fa3165b8bab80037c5b
Mayank Sharma <33985739+mayank-17@users.noreply.github.com>
2018-07-27 17:11:42 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,84 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+// AngularFire Imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+// Component Imports
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+// Service Imports
+import { ClientService } from './services/client.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+import { RegisterGuard } from './guards/register.guard';
+import { SettingsService } from './services/settings.service';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path:'', component:DashboardComponent, canActivate:[AuthGuard]},
+  {path:'register', component:RegisterComponent, canActivate:[RegisterGuard]},
+  {path:'login', component:LoginComponent},
+  {path:'add-client', component:AddClientComponent, canActivate:[AuthGuard]},
+  {path:'client/:id', component:ClientDetailsComponent, canActivate:[AuthGuard]},
+  {path:'edit-client/:id', component:EditClientComponent, canActivate:[AuthGuard]},
+  {path:'settings', component:SettingsComponent, canActivate:[AuthGuard]},
+  {path:'**', component:PageNotFoundComponent}
+];
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyD0IbOH2AXbIOet5ovW0_1uBFxIYrTqLTY',
+  authDomain: "clientpanel-9bec8.firebaseapp.com",
+  databaseURL: 'https://clientpanel-9bec8.firebaseio.com',
+  storageBucket: 'clientpanel-9bec8.appspot.com',
+  messagingSenderId: '160924906148'
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    FlashMessagesModule,
+  ],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService,
+    AuthService,
+    AuthGuard,
+    RegisterGuard,
+    SettingsService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
